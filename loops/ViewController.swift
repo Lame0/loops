25e5a69a7e2ace8d00d0eae2071bdc62f7f6f8f8
Damian <damianna070@gmail.com>
2022-10-26 10:15:58 -0600

    mvp

--- loops/ViewController.swift
+++ loops/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var textview: UITextView!
     var output=""
     
-  
+    
     
     
     override func viewDidLoad() {
@@ -24,40 +24,58 @@ class ViewController: UIViewController {
         
         
     }
-
+    
     
     @IBAction func Clear(_ sender: Any) {
         output=""
         textview.text=output
     }
     
-    @IBAction func button3(_ sender: Any) {
+    @IBAction func button3(_ sender: Any){
+        var drinks = ["tea","coffe","milk","soda"]
+        var output = ""
+        for i in 0..<3{
+            output += "\(drinks[i])\n"
+        }
+        textview.text=output
+        
     }
+    
     
     
     @IBAction func button5(_ sender: Any) {
+        var contact = ["Name":"Tom","Address":"123 Fake Street","Phone":"123-1234"]
+        var output=""
+        for (key,value) in contact{
+            output+="\(key): \(value)\n"
+        }
+        textview.text=output
     }
     
     @IBAction func button4(_ sender: Any) {
-    }
-    
-    @IBAction func button2(_ sender: Any) {
-        var veggies = ["tomato","carrot","pea","","celery"]
+        var snacks = ["chips","granola","nuts","popcorn"]
         var output = ""
-        for i in 0...3{
-        output += "\(veggies[i])\n"
+        for i in 0..<snacks.count{output += "\(snacks[i])\n"
         }
         textview.text=output
     }
-    
-    @IBAction func button1(_ sender: Any) {
-    
-        var fruit=["apple","pear","peach","strawberry"]
-        for item in fruit{
-            output+="\(item)\n"
+        @IBAction func button2(_ sender: Any) {
+            var veggies = ["tomato","carrot","pea","","celery"]
+            var output = ""
+            for i in 0...3{
+                output += "\(veggies[i])\n"
+            }
+            textview.text=output
         }
-        textview.text=output
-        print(output)
+        
+        @IBAction func button1(_ sender: Any) {
+            
+            var fruit=["apple","pear","peach","strawberry"]
+            for item in fruit{
+                output+="\(item)\n"
+            }
+            textview.text=output
+            print(output)
+        }
     }
-}
 
